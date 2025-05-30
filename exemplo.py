b883e4cfc7bee1545dc632c6c8393f21a3650697
brohi <brohigor@gmail.com>
2025-05-30 18:11:53 -0300

    primeiro exemplo funcionando

--- exemplo.py
+++ exemplo.py
@@ -12,13 +12,14 @@ df = pd.DataFrame({
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
 
+# Criando o grafico
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
+    html.H1(children='Faturamento das lojas'),
+    html.H2(children='grafico com o faturamento de todos os produtos separados por lojas'),
     html.Div(children='''
-        Dash: A web application framework for your data.
+        OBS: Este grafico mostra a quantidade de itens vendidos, não o faturamento
     '''),
 
     dcc.Graph(
